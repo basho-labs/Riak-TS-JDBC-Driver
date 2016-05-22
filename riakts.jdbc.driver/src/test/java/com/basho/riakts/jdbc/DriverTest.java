f1473074ca21588960cbf93570c348cd8ee141e7
Craig Vitter <craig.vitter@gmail.com>
2016-05-22 09:19:57 -0400

    Add driver and connection tests

---
+++ riakts.jdbc.driver/src/test/java/com/basho/riakts/jdbc/DriverTest.java
@@ -0,0 +1,35 @@
+package com.basho.riakts.jdbc;
+
+import junit.framework.TestCase;
+
+public class DriverTest extends TestCase {
+
+	public void testConnect() {
+		fail("Not yet implemented");
+	}
+
+	public void testAcceptsURL() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetPropertyInfo() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMajorVersion() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMinorVersion() {
+		fail("Not yet implemented");
+	}
+
+	public void testJdbcCompliant() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetParentLogger() {
+		fail("Not yet implemented");
+	}
+
+}
