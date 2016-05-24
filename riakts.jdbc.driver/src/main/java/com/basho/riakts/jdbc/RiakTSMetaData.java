cb275bf4f66d743fb21882ad595e7a9e35fc7e87
Craig Vitter <craig.vitter@gmail.com>
2016-05-24 17:38:54 -0400

    Update implemented features

--- riakts.jdbc.driver/src/main/java/com/basho/riakts/jdbc/RiakTSMetaData.java
+++ riakts.jdbc.driver/src/main/java/com/basho/riakts/jdbc/RiakTSMetaData.java
@@ -9,6 +9,8 @@ public class RiakTSMetaData implements java.sql.DatabaseMetaData {
 	
 	private static String databaseProductName = "Riak TS (Time Series)";
 	private static String databaseProductVersion = "1.3";
+	private static int dbMajorVersion = 1;
+	private static int dbMinorVersion = 3;
 	private static String driverProductName = "Riak TS JDBC";
 	private static String driverProductVersion = "0.1";
 	private static int driverMajorVersion = 0;
@@ -288,371 +290,291 @@ public class RiakTSMetaData implements java.sql.DatabaseMetaData {
 	}
 
 	public boolean isCatalogAtStart() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public String getCatalogSeparator() throws SQLException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public boolean supportsSchemasInDataManipulation() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSchemasInProcedureCalls() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSchemasInTableDefinitions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSchemasInIndexDefinitions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSchemasInPrivilegeDefinitions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsCatalogsInDataManipulation() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsCatalogsInProcedureCalls() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsCatalogsInTableDefinitions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsCatalogsInIndexDefinitions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsCatalogsInPrivilegeDefinitions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsPositionedDelete() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsPositionedUpdate() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSelectForUpdate() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsStoredProcedures() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSubqueriesInComparisons() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSubqueriesInExists() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSubqueriesInIns() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsSubqueriesInQuantifieds() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsCorrelatedSubqueries() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsUnion() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsUnionAll() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsOpenCursorsAcrossCommit() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsOpenCursorsAcrossRollback() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsOpenStatementsAcrossCommit() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsOpenStatementsAcrossRollback() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public int getMaxBinaryLiteralLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxCharLiteralLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxColumnNameLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxColumnsInGroupBy() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxColumnsInIndex() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxColumnsInOrderBy() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxColumnsInSelect() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxColumnsInTable() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxConnections() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxCursorNameLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxIndexLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxSchemaNameLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxProcedureNameLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxCatalogNameLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxRowSize() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public boolean doesMaxRowSizeIncludeBlobs() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public int getMaxStatementLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxStatements() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxTableNameLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getMaxTablesInSelect() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 	public int getMaxUserNameLength() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getDefaultTransactionIsolation() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public boolean supportsTransactions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public boolean supportsTransactionIsolationLevel(int level)
-			throws SQLException {
-		// TODO Auto-generated method stub
+	public boolean supportsTransactionIsolationLevel(int level) throws SQLException {
 		return false;
 	}
 
-	public boolean supportsDataDefinitionAndDataManipulationTransactions()
-			throws SQLException {
-		// TODO Auto-generated method stub
+	public boolean supportsDataDefinitionAndDataManipulationTransactions() throws SQLException {
 		return false;
 	}
 
-	public boolean supportsDataManipulationTransactionsOnly()
-			throws SQLException {
-		// TODO Auto-generated method stub
+	public boolean supportsDataManipulationTransactionsOnly() throws SQLException {
 		return false;
 	}
 
 	public boolean dataDefinitionCausesTransactionCommit() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean dataDefinitionIgnoredInTransactions() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public ResultSet getProcedures(String catalog, String schemaPattern,
-			String procedureNamePattern) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultSet getProcedures(String catalog, String schemaPattern, String procedureNamePattern) throws SQLException {
+		throw new UnsupportedOperationException();
 	}
 
-	public ResultSet getProcedureColumns(String catalog, String schemaPattern,
-			String procedureNamePattern, String columnNamePattern)
+	public ResultSet getProcedureColumns(String catalog, String schemaPattern, String procedureNamePattern, String columnNamePattern)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
-	public ResultSet getTables(String catalog, String schemaPattern,
-			String tableNamePattern, String[] types) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultSet getTables(String catalog, String schemaPattern, String tableNamePattern, String[] types) throws SQLException {
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getSchemas() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getCatalogs() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getTableTypes() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
-	public ResultSet getColumns(String catalog, String schemaPattern,
-			String tableNamePattern, String columnNamePattern)
+	public ResultSet getColumns(String catalog, String schemaPattern, String tableNamePattern, String columnNamePattern)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getColumnPrivileges(String catalog, String schema,
 			String table, String columnNamePattern) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getTablePrivileges(String catalog, String schemaPattern,
 			String tableNamePattern) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getBestRowIdentifier(String catalog, String schema,
 			String table, int scope, boolean nullable) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getVersionColumns(String catalog, String schema,
 			String table) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
-	public ResultSet getPrimaryKeys(String catalog, String schema, String table)
-			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultSet getPrimaryKeys(String catalog, String schema, String table) throws SQLException {
+		throw new UnsupportedOperationException();
 	}
 
-	public ResultSet getImportedKeys(String catalog, String schema, String table)
-			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultSet getImportedKeys(String catalog, String schema, String table) throws SQLException {
+		throw new UnsupportedOperationException();
 	}
 
-	public ResultSet getExportedKeys(String catalog, String schema, String table)
-			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public ResultSet getExportedKeys(String catalog, String schema, String table) throws SQLException {
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getCrossReference(String parentCatalog,
 			String parentSchema, String parentTable, String foreignCatalog,
 			String foreignSchema, String foreignTable) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getTypeInfo() throws SQLException {
@@ -667,70 +589,57 @@ public class RiakTSMetaData implements java.sql.DatabaseMetaData {
 	}
 
 	public boolean supportsResultSetType(int type) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	public boolean supportsResultSetConcurrency(int type, int concurrency)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	public boolean ownUpdatesAreVisible(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean ownDeletesAreVisible(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean ownInsertsAreVisible(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean othersUpdatesAreVisible(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean othersDeletesAreVisible(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean othersInsertsAreVisible(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean updatesAreDetected(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean deletesAreDetected(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean insertsAreDetected(int type) throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsBatchUpdates() throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public ResultSet getUDTs(String catalog, String schemaPattern,
 			String typeNamePattern, int[] types) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public Connection getConnection() throws SQLException {
@@ -739,63 +648,52 @@ public class RiakTSMetaData implements java.sql.DatabaseMetaData {
 	}
 
 	public boolean supportsSavepoints() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsNamedParameters() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsMultipleOpenResults() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsGetGeneratedKeys() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public ResultSet getSuperTypes(String catalog, String schemaPattern,
 			String typeNamePattern) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getSuperTables(String catalog, String schemaPattern,
 			String tableNamePattern) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getAttributes(String catalog, String schemaPattern,
 			String typeNamePattern, String attributeNamePattern)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public boolean supportsResultSetHoldability(int holdability)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	public int getResultSetHoldability() throws SQLException {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	public int getDatabaseMajorVersion() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return dbMajorVersion;
 	}
 
 	public int getDatabaseMinorVersion() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		return dbMinorVersion;
 	}
 
 	public int getJDBCMajorVersion() throws SQLException {
@@ -814,33 +712,27 @@ public class RiakTSMetaData implements java.sql.DatabaseMetaData {
 	}
 
 	public boolean locatorsUpdateCopy() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean supportsStatementPooling() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public RowIdLifetime getRowIdLifetime() throws SQLException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public ResultSet getSchemas(String catalog, String schemaPattern)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public boolean supportsStoredFunctionsUsingCallSyntax() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean autoCommitFailureClosesAllResultSets() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -851,26 +743,22 @@ public class RiakTSMetaData implements java.sql.DatabaseMetaData {
 
 	public ResultSet getFunctions(String catalog, String schemaPattern,
 			String functionNamePattern) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getFunctionColumns(String catalog, String schemaPattern,
 			String functionNamePattern, String columnNamePattern)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public ResultSet getPseudoColumns(String catalog, String schemaPattern,
 			String tableNamePattern, String columnNamePattern)
 			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	public boolean generatedKeyAlwaysReturned() throws SQLException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
