973a070cf3618be042b54f7bb4b4f3273b8e4f49
Craig Vitter <craig.vitter@gmail.com>
2016-08-29 10:12:46 -0400

    Update license headers

--- riakts.jdbc.driver/src/main/java/com/basho/riakts/jdbc/PreparedStatement.java
+++ riakts.jdbc.driver/src/main/java/com/basho/riakts/jdbc/PreparedStatement.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright (C) 2016 Craig Vitter - https://github.com/cvitter
+ * Copyright (C) 2016 Basho Technologies Inc
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
